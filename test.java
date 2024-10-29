1bea4fd40f22abae05c13de18525c6a5958e400b
Artun <owlartpuni@gmail.com>
2024-10-28 21:24:54 -0300

    test

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class test {
 
     public static void main(String []args) {
-       System.out.println("Hello BoilerplateWorldddwaddwadwawadawdadddwadwadawdadwadawdwwddd");
+       System.out.println("Hello BoilerplateWorldddwaddwadwawadawdadddwadwadawdadwadawdwwddwadwadd");
     }
  }
